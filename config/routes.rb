c532586adaaaf0df38141b19c2f05a8210a22b7a
Jordan Lewis <jordan@lewis.io>
2013-06-10 22:33:17 +1000

    Added social links to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,20 @@ module RouteHelpers
   def register_url
     "http://www.meetup.com/Responsive-Web-Melbourne/"
   end
+
+  def linkedin_url
+    "http://www.linkedin.com/groups/Be-Responsive-5022623"
+  end
+
+  def beresponsive_email
+    "contact@beresponsive.io"
+  end
+
+  def twitter_url
+    "https://twitter.com/beresponsiveio"
+  end
+
+  def facebook_url
+    "https://www.facebook.com/pages/Be-Responsive/577970475568743"
+  end
 end
